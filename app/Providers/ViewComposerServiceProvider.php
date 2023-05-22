1b2d30fb7130aa7134628ac919048526422d01ec
s-inoue0108 <134176915+s-inoue0108@users.noreply.github.com>
2023-05-22 20:08:17 +0900

    Update ViewComposerServiceProvider.php

--- app/Providers/ViewComposerServiceProvider.php
+++ app/Providers/ViewComposerServiceProvider.php
@@ -6,7 +6,6 @@ use Illuminate\Support\ServiceProvider;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Schema;
 
 use App\Models\Status;
 
@@ -25,11 +24,9 @@ class ViewComposerServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (Schema::hasTable('statuses')) {
-            // my_status変数を共通化
-            View::composer('*', function ($view)  {
-                $view->with('my_status', Status::find(Auth::id()));
-            });//
-        }
+        // my_status変数を共通化
+        View::composer('*', function ($view)  {
+            $view->with('my_status', Status::find(Auth::id()));
+        });//
     }
 }
