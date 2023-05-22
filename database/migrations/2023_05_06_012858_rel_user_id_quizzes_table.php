52b83a66f042d5d37954d29a22647cc86553d73c
s-inoue0108 <134176915+s-inoue0108@users.noreply.github.com>
2023-05-22 19:27:50 +0900

    Update 2023_05_06_012858_rel_user_id_quizzes_table.php

--- database/migrations/2023_05_06_012858_rel_user_id_quizzes_table.php
+++ database/migrations/2023_05_06_012858_rel_user_id_quizzes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('Quizzes', function (Blueprint $table) {
+        Schema::table('quizzes', function (Blueprint $table) {
             $table->foreignId('user_id'); //
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('Quizzes', function (Blueprint $table) {
+        Schema::table('quizzes', function (Blueprint $table) {
             $table->dropColumn('user_id'); //
         });
     }
